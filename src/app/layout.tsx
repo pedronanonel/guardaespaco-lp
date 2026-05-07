b55d06f113525615a4d6d4ef0ac2ea423faad6bf
Pedro Manoel de Souza Neto <pedromanoeldesouzaneto@Pedros-MacBook-Air.local>
2026-05-07 10:58:27 -0300

    chore: replace google tag manager with new gtag tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,17 +28,19 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={cn("scroll-smooth")}>
       <head>
-        {/* Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11250218538"></script>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-58GFXM39');`,
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-11250218538');
+            `,
           }}
         />
-        {/* End Google Tag Manager */}
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
         <style dangerouslySetInnerHTML={{ __html: `
           :root {
